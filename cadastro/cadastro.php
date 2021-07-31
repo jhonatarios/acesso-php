c100a8351bc89d0867aa445bc36ed7a9f3fe904a
Jhonata Rios <72048481+jhonatarios@users.noreply.github.com>
2021-07-31 07:12:53 -0300

    update1

--- cadastro/cadastro.php
+++ cadastro/cadastro.php
@@ -1,41 +1,28 @@
 <?php
+// Preenchidos
 $nome = $_POST['nome'];
 $usuario = $_POST['usuario'];
 $email = $_POST['email'];
 $senha = md5($_POST['senha']);
 
-$servername = "localhost:3306";
-$database = "acesso_php";
-$username = "root";
-$password = "root";
+// Incluir conexao ao db
+$conn= new mysqli_connect ("localhost:3306","root","root","acesso_php") or die(mysqli_error($conn));
 
-// Criar conexao
-$conn = mysqli_connect($servername, $username, $password, $database);
-// Check conexao
-if (!$conn) {
-      die("Connection failed: " . mysqli_connect_error());
-}
- 
-echo "Connected successfully";
-
-
-      // checar nulos
-      if(empty($_POST['nome']) || empty($_POST['usuario']) || 
+// Checar -Preenchidos- nulos
+if(empty($_POST['nome']) || empty($_POST['usuario']) || 
    empty($_POST['email']) || empty($_POST['senha'])) {
-  echo "<script language='javascript' type='text/javascript'>alert('Ainda há campos sem preencher');window.location.href='index.html';</script>";
-  exit;
-}else{
-
-      // entrar
+      echo "<script language='javascript' type='text/javascript'>alert('Ainda há campos sem preencher');window.location.href='index.html';</script>";
+      exit;
+}else{     
       $sql = "INSERT INTO usuarios(nome, usuario, email, senha) VALUES ('$nome','$usuario','$email','$senha')";
-      if (mysqli_query($conn, $sql)) {
+
+      if(mysqli_query($conn, $sql)){
             echo "Usuario cadastrado com sucesso.<script>location.href = '../entrar';</script>";
-      } else {
+      }else{
             echo "<script language='javascript' type='text/javascript'>alert('Usuario ou email já cadastrados no sistema, tente um usuario ou email diferentes.');window.location.href='index.html';</script>" . mysqli_error($conn);
       }
+
+      // Desconectar
       mysqli_close($conn);
-
-
-
 }
 ?>
--- entrar/entrar.php
+++ entrar/entrar.php
@@ -1,18 +1,21 @@
 <?php 
-  $usuario = $_POST['usuario'];
-  $entrar = $_POST['entrar'];
-  $senha = md5($_POST['senha']);
-  $connect = mysqli_connect('localhost:3306','root','root','acesso_php');
+$usuario = $_POST['usuario'];
+$entrar = $_POST['entrar'];
+$senha = md5($_POST['senha']);
+
+$conn= new mysqli_connect ("localhost:3306","root","root","acesso_php") or die(mysqli_error($conn));
   
-    if (isset($entrar)) {
+if (isset($entrar)) {
             
-      $verifica = mysqli_query($connect, "SELECT * FROM usuarios WHERE usuario = '$usuario' AND senha = '$senha'") or die("Erro ao selecionar");
-        if (mysqli_num_rows($verifica)<=0){
-          echo"<script language='javascript' type='text/javascript'>alert('Usuario e/ou senha incorretos');window.location.href='../entrar';</script>";
-          die();
-        }else{
-          setcookie("usuario",$usuario, time()+(24*60*60*7), '/' );
-          header("Location:../inicio");
-        }
+  $verifica = mysqli_query($connect, "SELECT * FROM usuarios WHERE usuario = '$usuario' AND senha = '$senha'") or die("Erro ao selecionar");
+  if (mysqli_num_rows($verifica)<=0){
+    echo"<script language='javascript' type='text/javascript'>alert('Usuario e/ou senha incorretos');window.location.href='../entrar';</script>";
+    die();
+    }else{
+      setcookie("usuario",$usuario, time()+(24*60*60*7), '/' );
+      header("Location:../inicio");
     }
+  // Desconectar
+  mysqli_close($conn);
+}
 ?>
